ce12d95f4f92e06f384a981f807a86c0ce983dd1
wandersonwendel <wandsolopes@alu.ufc.br>
2024-01-11 19:15:13 -0300

    Adiciona itens à lista - Implementa a lógica para adicionar um novo item à lista quando o formulário é enviado - Condição para evitar adição de itens vazios ou nulos - Atualiza a renderização condicional da lista com base no comprimento e nos itens existentes, aplicando classes de estilo conforme necessário - Renderiza os textos dos itens do input dentro do elemento <span>

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,28 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ToDoList.css";
 
 function ToDoList(){
+
+    // Estado para armazenar a lista de itens
+    const[lista, setLista] = useState([]);
+
+    // Estado para controlar a entrada de um novo item
+    const [novoItem, setNovoItem] = useState("");
+
+    function adicionaItem(form){
+        // Impede o comportamento padrão do navegador de recarregar a página quando o formulário é enviado
+        form.preventDefault();
+
+        if(!novoItem){ // Se 'novoItem' ainda estiver vazio ou nulo, não ocorra eventos
+            return;
+        }
+        // Senão, atualiza valor de 'lista', atribuindo com 'novoItem' o que foi digitado no input e foca no campo vazio
+        setLista([...lista, {text: novoItem, isCompleted: false }])
+        setNovoItem("");
+        document.getElementById('input-entrada').focus();
+    }
+
     return (
         <div>
             <h1>Lista de Tarefas</h1>
-            <form>
-                <input 
+            <form onSubmit={adicionaItem}> {/* Chama a função adicionaItem() quando o formulário for enviado. */}
+                <input
+                id="input-entrada"
                 type="text"
+                value={novoItem} // A propriedade 'value' é vinculada ao estado 'novoItem' para criar um input controlado
+                onChange={(e)=>{setNovoItem(e.target.value)}} // Atualiza o estado novoItem com o valor atual do input.
                 placeholder="Digite suas tarefas"
                 />
                 <button className="add" type="submit">Add</button>
             </form>
             <div className="listaTarefas">
-                <div className="item">
-                    <span>Tarefa de exemplo</span>
-                    <button className="del">Deletar</button>
+                <div>
+                    {
+                        // Renderiza uma imagem se a lista estiver vazia; caso contrário, mapeia a lista renderizando os itens em 'span'
+                        lista.length < 1
+                        ?
+                        <img />
+                        :
+                        lista.map((item, index)=>(
+                            // Recebe classe "item completo" se 'isCompleted' for verdadeiro, caso contrário, renderiza com a classe "item".
+                            <div className={item.isCompleted ? "item completo" : "item"}>
+                                <span>{item.text}</span>
+                                <button className="del">Deletar</button>
+                            </div>
+                        ))
+                    }
+                    <button className="deleteAll">Deletar Tudo</button>
                 </div>
-
-                <div className="item completo">
-                    <span>Tarefa de exemplo</span>
-                    <button className="del">Deletar</button>
-                </div>
-                <button className="deleteAll">Deletar Tudo</button>
             </div>
         </div>
     )
